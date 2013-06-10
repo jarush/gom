6a864f438fc692aaf947618c556cde9d7705fe1b
Jason Rush <rush0033@hotmail.com>
2013-06-09 19:30:06 -0500

    Made garage door settings persistant

--- web/templates/settings-garagedoors.php
+++ web/templates/settings-garagedoors.php
@@ -1,31 +1,67 @@
 <?php
 include 'header.inc.php';
-include 'settings-tabs.inc.php'
+include 'settings-tabs.inc.php';
+
+// Get the network configuration
+$c = new Config('garagedoors.properties');
 ?>
 
-<form>
+<?php
+if (isset($status)) {
+  echo '<div class="alert alert-success">';
+  echo '<button type="button" class="close" data-dismiss="alert">&times;</button>';
+  echo $status;
+  echo '</div>';
+}
+?>
+
+<form id="form" method="post">
+  <p>
+    <input type="button" class="btn" id="add_door" value="Add Door" />
+  </p>
+
   <table class="table table-striped table-bordered">
     <thead>
       <tr>
-        <th>Door</th>
-        <th>Enabled</th>
+        <th>Name</th>
         <th>Sensor GPIO</th>
         <th>Relay GPIO</th>
+        <th></th>
       </tr>
     </thead>
     <tbody>
+<?php
+
+for ($i = 0; $i < 10; $i++) {
+  $name = $c->get('name' . $i, null);
+  $sensorGpio = $c->get('sensorGpio' . $i, null);
+  $relayGpio = $c->get('relayGpio' . $i, null);
+
+  if ($name == null || $sensorGpio == null || $relayGpio == null) {
+    continue;
+  }
+
+?>
       <tr>
-        <td>1</td>
-        <td>Enabled</td>
-        <td>GPIO</td>
-        <td>GPIO</td>
+        <td>
+          <input type="text" name="name[]" value="<?php echo $name; ?>"
+                 required="required" />
+        </td>
+        <td>
+          <input type="text" name="sensorGpio[]" value="<?php echo $sensorGpio; ?>"
+                 required="required" pattern="\d+" />
+        </td>
+        <td>
+          <input type="text" name="relayGpio[]" value="<?php echo $relayGpio; ?>"
+                 required="required" pattern="\d+" />
+        </td>
+        <td>
+          <input type='button' id='remove' value='Remove' />
+        </td>
       </tr>
-      <tr>
-        <td>2</td>
-        <td>Enabled</td>
-        <td>GPIO</td>
-        <td>GPIO</td>
-      </tr>
+<?php
+}
+?>
     </tbody>
   </table>
 
@@ -34,5 +70,47 @@ include 'settings-tabs.inc.php'
     <button type="reset" class="btn">Reset</button>
   </div>
 </form>
+
+<script type="text/javascript">
+  $('#add_door').click(function() {
+    numDoors = 0;
+    $('tbody').append(
+      $('<tr>').append([
+        $('<td>').append(
+          $('<input>').attr({
+            'type' : 'text',
+            'name' : 'name[]'
+          })
+        ),
+        $('<td>').append(
+          $('<input>').attr({
+            'type' : 'text',
+            'name' : 'sensorGpio[]'
+          })
+        ),
+        $('<td>').append(
+          $('<input>').attr({
+            'type' : 'text',
+            'name' : 'relayGpio[]'
+          })
+        ),
+        $('<td>').append(
+          $('<input>').attr({
+            'type'  : 'button',
+            'id'    : 'remove_door',
+            'class' : 'btn',
+            'value' : 'Remove'
+          }).click(function() {
+            $(this).parents('tr').remove();
+          })
+        ),
+      ])
+    );
+  });
+
+  $('#remove_door').click(function() {
+    $(this).parents('tr').remove();
+  });
+</script>
 
 <?php include 'footer.inc.php'; ?>
--- web/views/settings-garagedoors.view.php
+++ web/views/settings-garagedoors.view.php
@@ -8,4 +8,45 @@ $app->get('/settings/garagedoors', $authMw, function () use($app) {
   ));
 });
 
+$app->post('/settings/garagedoors', $authMw, function () use($app) {
+  $config = new Config();
+  $req = $app->request();
+
+  $name = $req->post('name');
+  $sensorGpio = $req->post('sensorGpio');
+  $relayGpio = $req->post('relayGpio');
+
+  $n = count($name);
+
+  $door = 0;
+  for ($i = 0; $i < $n; $i++) {
+    // Skip the door is no pin is set
+    if (!isset($name[$i]) || !isset($sensorGpio[$i]) || !isset($relayGpio[$i])) {
+      continue;
+    }
+
+    $config->set('name' . $door, $name[$i]);
+    $config->set('sensorGpio' . $door, $sensorGpio[$i]);
+    $config->set('relayGpio' . $door, $relayGpio[$i]);
+
+    $door++;
+  }
+
+  if ($config->save('garagedoors.properties') === FALSE) {
+    $app->render('settings-garagedoors.php', array(
+      'site'   => 'Garage',
+      'title'  => 'Settings',
+      'tab'    => 'Network',
+      'status' => 'Failed to save settings',
+    ));
+    return;
+  }
+
+  $app->render('settings-garagedoors.php', array(
+    'site'   => 'Garage',
+    'title'  => 'Settings',
+    'tab'    => 'Network',
+    'status' => 'Settings saved successfully',
+  ));
+});
 ?>
