f08079b20358104361fb956f7e39cd41bd892123
Jason Rush <rush0033@hotmail.com>
2014-03-12 21:01:49 -0500

    Cleaned up table formatting

--- web/templates/settings-garagedoors.php
+++ web/templates/settings-garagedoors.php
@@ -22,16 +22,17 @@ if (isset($status)) {
       <input type="button" class="btn btn-default" id="add_door" value="Add Door" />
     </p>
 
-    <table class="table table-striped table-bordered">
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Sensor GPIO</th>
-          <th>Relay GPIO</th>
-          <th></th>
-        </tr>
-      </thead>
-      <tbody>
+    <div class="panel panel-default">
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Sensor GPIO</th>
+            <th>Relay GPIO</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
 <?php
 
 $numGarageDoors = 0;
@@ -39,33 +40,34 @@ foreach ($garageDoors as $gd) {
   $numGarageDoors++;
 
 ?>
-        <tr>
-          <td>
-            <input type="text" name="name[]" class="form-control"
-                   value="<?php echo $gd->getName(); ?>"
-                   required="required" />
-          </td>
-          <td>
-            <input type="text" name="sensorGpio[]" class="form-control"
-                   value="<?php echo $gd->getSensorGpio(); ?>"
-                   required="required" pattern="\d+" />
-          </td>
-          <td>
-            <input type="text" name="relayGpio[]" class="form-control"
-                   value="<?php echo $gd->getRelayGpio(); ?>"
-                   required="required" pattern="\d+" />
-          </td>
-          <td class="text-center">
-            <input type="button" class="btn btn-danger" name="remove" value="Remove" />
-          </td>
-        </tr>
+          <tr>
+            <td>
+              <input type="text" name="name[]" class="form-control"
+                     value="<?php echo $gd->getName(); ?>"
+                     required="required" />
+            </td>
+            <td>
+              <input type="text" name="sensorGpio[]" class="form-control"
+                     value="<?php echo $gd->getSensorGpio(); ?>"
+                     required="required" pattern="\d+" />
+            </td>
+            <td>
+              <input type="text" name="relayGpio[]" class="form-control"
+                     value="<?php echo $gd->getRelayGpio(); ?>"
+                     required="required" pattern="\d+" />
+            </td>
+            <td class="text-center">
+              <input type="button" class="btn btn-danger" name="remove" value="Remove" />
+            </td>
+          </tr>
 <?php
 }
 ?>
-      </tbody>
-    </table>
+        </tbody>
+      </table>
+    </div>
 
-    <div class="form-actions">
+    <div class="form-group">
       <button type="submit" class="btn btn-primary">Save</button>
       <button type="reset" class="btn">Reset</button>
     </div>
