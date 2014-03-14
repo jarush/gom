9761e3dfbde192a7812ac06264198a83b670843b
Jason Rush <rush0033@hotmail.com>
2014-03-14 11:52:05 -0500

    Removed wlan from settings page

--- web/templates/settings-network.php
+++ web/templates/settings-network.php
@@ -6,20 +6,6 @@ include 'funcs.inc.php';
 // Get the network configuration
 $c = new Config('network.properties');
 
-// Check if the wlan interface and wlan dhcp are enabled
-$wlan = $c->get('wlan', null) == 'enabled';
-$wlan_dhcp = $c->get('wlan_dhcp', null) == 'enabled';
-if (!$wlan) {
-  $wlan_disabled = 'disabled';
-  $wlan_dhcp_disabled = 'disabled';
-} else if ($wlan_dhcp) {
-  $wlan_disabled = '';
-  $wlan_dhcp_disabled = 'disabled';
-} else {
-  $wlan_disabled = '';
-  $wlan_dhcp_disabled = '';
-}
-
 // Check if the eth interface and eth dhcp are enabled
 $eth = $c->get('eth', null) == 'enabled';
 $eth_dhcp = $c->get('eth_dhcp', null) == 'enabled';
@@ -43,102 +29,6 @@ if (!$eth) {
   ?>
 
   <form class="form-horizontal" method="post">
-    <fieldset>
-      <legend>Wireless Interface</legend>
-      <div class="form-group">
-        <label class="col-sm-2 control-label" for="wlan">Interface</label>
-        <div class="col-sm-10">
-          <label class="radio-inline">
-            <input type="radio" name="wlan" value="enabled"
-                   <?php echo ($wlan) ? 'checked' : ''?>/>Enabled
-          </label>
-          <label class="radio-inline">
-            <input type="radio" name="wlan" value="disabled"
-                   <?php echo (!$wlan) ? 'checked' : ''?>/>Disabled
-          </label>
-        </div>
-      </div>
-      <div class="form-group">
-        <label class="col-sm-2 control-label" for="wlan_ssid">SSID</label>
-        <div class="col-sm-10">
-          <input type="text" class="form-control" name="wlan_ssid"
-                 placeholder="SSID" required="required"
-                 <?php echo $wlan_disabled; ?>
-                 value="<?php echo $c->get('wlan_ssid', ''); ?>"/>
-        </div>
-      </div>
-      <div class="form-group">
-        <label class="col-sm-2 control-label" for="wlan_security_mode">Security Mode</label>
-        <div class="col-sm-10">
-          <select name="wlan_security_mode" class="form-control"
-                  <?php echo $wlan_disabled; ?>>
-          <?php
-            $modes = array('WPA2 Personal', 'WPA Personal', 'WEP');
-            $selected_mode = $c->get('wlan_security_mode', null);
-            foreach ($modes as $mode) {
-              $selected = $mode == $selected_mode ? 'selected' : '';
-              echo "<option $selected>$mode</option>";
-            }
-          ?>
-          </select>
-        </div>
-      </div>
-      <div class="form-group">
-        <label class="col-sm-2 control-label" for="wlan_key">Key</label>
-        <div class="col-sm-10">
-          <input type="text" name="wlan_key" class="form-control"
-                 placeholder="Key" required="required"
-                 <?php echo $wlan_disabled; ?>
-                 value="<?php echo $c->get('wlan_key', ''); ?>"/>
-        </div>
-      </div>
-      <div class="form-group">
-        <label class="col-sm-2 control-label" for="wlan_dhcp">DHCP</label>
-        <div class="col-sm-10">
-          <label class="radio-inline">
-            <input type="radio" name="wlan_dhcp" value="enabled"
-                 <?php echo $wlan_disabled; ?>
-                 <?php echo ($wlan_dhcp) ? 'checked' : ''?>/>Enabled
-          </label>
-          <label class="radio-inline">
-            <input type="radio" name="wlan_dhcp" value="disabled"
-                 <?php echo $wlan_disabled; ?>
-                 <?php echo (!$wlan_dhcp) ? 'checked' : ''?>/>Disabled
-          </label>
-        </div>
-      </div>
-      <div class="form-group">
-        <label class="col-sm-2 control-label" for="wlan_static_address">Static Address</label>
-        <div class="col-sm-10">
-          <input type="text" name="wlan_static_address" class="form-control"
-                 placeholder="IP Address" required="required"
-                 pattern="\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}"
-                 <?php echo $wlan_dhcp_disabled; ?>
-                 value="<?php echo $c->get('wlan_static_address', ''); ?>"/>
-        </div>
-      </div>
-      <div class="form-group">
-        <label class="col-sm-2 control-label" for="wlan_static_netmask">Netmask</label>
-        <div class="col-sm-10">
-          <input type="text" name="wlan_static_netmask" class="form-control"
-                 placeholder="Netmask" required="required"
-                 pattern="\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}"
-                 <?php echo $wlan_dhcp_disabled; ?>
-                 value="<?php echo $c->get('wlan_static_netmask', ''); ?>"/>
-        </div>
-      </div>
-      <div class="form-group">
-        <label class="col-sm-2 control-label" for="wlan_static_gateway">Gateway</label>
-        <div class="col-sm-10">
-          <input type="text" name="wlan_static_gateway" class="form-control"
-                 placeholder="Gateway Address" required="required"
-                 pattern="\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}"
-                 <?php echo $wlan_dhcp_disabled; ?>
-                 value="<?php echo $c->get('wlan_static_gateway', ''); ?>"/>
-        </div>
-      </div>
-    </fieldset>
-
     <fieldset class="disable">
       <legend>Ethernet Interface</legend>
       <div class="form-group">
@@ -210,14 +100,10 @@ if (!$eth) {
 
 <script type="text/javascript">
   // Enable/disable static input fields when DHCP is disabled/enabled
-  $("input[name='wlan_dhcp'][value='enabled']").click(function() { enable_dhcp('wlan'); });
-  $("input[name='wlan_dhcp'][value='disabled']").click(function() { disable_dhcp('wlan'); });
   $("input[name='eth_dhcp'][value='enabled']").click(function() { enable_dhcp('eth'); });
   $("input[name='eth_dhcp'][value='disabled']").click(function() { disable_dhcp('eth'); });
 
   // Enable/disable all input fields when interface is enabled/disabled
-  $("input[name='wlan'][value='enabled']").click(function() { enable_iface('wlan'); });
-  $("input[name='wlan'][value='disabled']").click(function() { disable_iface('wlan'); });
   $("input[name='eth'][value='enabled']").click(function() { enable_iface('eth'); });
   $("input[name='eth'][value='disabled']").click(function() { disable_iface('eth'); });
 
--- web/views/settings-network.view.php
+++ web/views/settings-network.view.php
@@ -12,46 +12,6 @@ $app->post('/settings/network', $authMw, function () use($app) {
   $config = new Config('network.properties');
   $req = $app->request();
 
-  $wlan = $req->post('wlan');
-  if (isset($wlan)) {
-    $config->set('wlan', $wlan);
-  }
-
-  $wlan_ssid = $req->post('wlan_ssid');
-  if (isset($wlan_ssid)) {
-    $config->set('wlan_ssid', $wlan_ssid);
-  }
-
-  $wlan_security_mode = $req->post('wlan_security_mode');
-  if (isset($wlan_security_mode)) {
-    $config->set('wlan_security_mode', $wlan_security_mode);
-  }
-
-  $wlan_key = $req->post('wlan_key');
-  if (isset($wlan_key)) {
-    $config->set('wlan_key', $wlan_key);
-  }
-
-  $wlan_dhcp = $req->post('wlan_dhcp');
-  if (isset($wlan_dhcp)) {
-    $config->set('wlan_dhcp', $wlan_dhcp);
-  }
-
-  $wlan_static_address = $req->post('wlan_static_address');
-  if (isset($wlan_static_address)) {
-    $config->set('wlan_static_address', $wlan_static_address);
-  }
-
-  $wlan_static_netmask = $req->post('wlan_static_netmask');
-  if (isset($wlan_static_netmask)) {
-    $config->set('wlan_static_netmask', $wlan_static_netmask);
-  }
-
-  $wlan_static_gateway = $req->post('wlan_static_gateway');
-  if (isset($wlan_static_gateway)) {
-    $config->set('wlan_static_gateway', $wlan_static_gateway);
-  }
-
   $eth = $req->post('eth');
   if (isset($eth)) {
     $config->set('eth', $eth);
