e3571126baaa4afd204d78e84a36835242620b60
Jason Rush <rush0033@hotmail.com>
2014-03-17 17:35:24 -0500

    Cleaned up post build script

---
+++ scripts/post-build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+TARGETDIR=$1
+
+rm -f $TARGETDIR/etc/os-release
+rm -f $TARGETDIR/etc/init.d/S20urandom
+
+cp $RPI_PATH/gomd/src/gomd.properties $TARGETDIR/etc/
+cp $RPI_PATH/gomd/src/gomd $TARGETDIR/usr/bin/
+
