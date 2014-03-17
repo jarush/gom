011a0770693b3bc1c268ee47748e8b0d7d8079f1
Jason Rush <rush0033@hotmail.com>
2014-03-17 07:34:14 -0500

    Added code to set username/password

--- web/templates/settings-management.php
+++ web/templates/settings-management.php
@@ -1,13 +1,53 @@
 <?php
 include 'header.inc.php';
-include 'settings-tabs.inc.php'
+include 'settings-tabs.inc.php';
+include 'funcs.inc.php';
+
+// Get the notifications configuration
+$c = new Config('login.properties');
+$username = $c->get('username', '');
+$password = $c->get('password', '');
+
 ?>
 
 <div class="tab-body">
+  <?php
+    if (isset($status)) {
+      showAlert($status, $message);
+    }
+  ?>
+
   <form class="form-horizontal" method="post">
-    <fieldset>
-      <legend>Administration Password</legend>
-    </fieldset>
+    <legend>Login</legend>
+
+    <div class="form-group">
+      <label class="col-sm-3 control-label" for="username">Username</label>
+      <div class="col-sm-9">
+        <input type="text" class="form-control" name="username"
+               placeholder="Username" required="required" autocomplete="off"
+               value="<?php echo $username; ?>" />
+      </div>
+    </div>
+
+    <div class="form-group">
+      <label class="col-sm-3 control-label" for="password1">Password</label>
+      <div class="col-sm-9">
+        <input type="password" class="form-control" name="password1"
+               placeholder="Password" required="required"
+               autocomplete="off"
+               value="<?php echo $password; ?>" />
+      </div>
+    </div>
+
+    <div class="form-group">
+      <label class="col-sm-3 control-label" for="password2">Repeat Password</label>
+      <div class="col-sm-9">
+        <input type="password" class="form-control" name="password2"
+               placeholder="Repeat Password" required="required"
+               autocomplete="off"
+               value="<?php echo $password; ?>" />
+      </div>
+    </div>
 
     <div class="form-actions">
       <button type="submit" class="btn btn-primary">Save</button>
--- web/views/settings-management.view.php
+++ web/views/settings-management.view.php
@@ -7,5 +7,56 @@ $app->get('/settings/management', $authMw, function () use($app) {
     'tab'   => 'Management',
   ));
 });
+$app->post('/settings/management', $authMw, function () use($app) {
+  $req = $app->request();
+
+  $username = $req->post('username');
+  $password1 = $req->post('password1');
+  $password2 = $req->post('password2');
+
+  $message = null;
+  if (!isset($username) || $username == '') {
+    $message = 'Missing required username field';
+  } else if (!isset($password1) || $password1 == '' ||
+      !isset($password2) || $password2 == '') {
+    $message = 'Missing required password field';
+  } else if ($password1 != $password2) {
+    $message = 'Passwords do not match';
+  }
+
+  if ($message != null) {
+    $app->render('settings-management.php', array(
+      'site'    => 'Garage',
+      'title'   => 'Settings',
+      'tab'     => 'Garage Doors',
+      'status'  => 'error',
+      'message' => $message,
+    ));
+    return;
+  }
+
+  $c = new Config('login.properties');
+  $c->set('username', $username);
+  $c->set('password', $password1);
+
+  if ($c->save('login.properties') === FALSE) {
+    $app->render('settings-management.php', array(
+      'site'    => 'Garage',
+      'title'   => 'Settings',
+      'tab'     => 'Garage Doors',
+      'status'  => 'error',
+      'message' => 'Failed to save garage door settings',
+    ));
+    return;
+  }
+
+  $app->render('settings-management.php', array(
+    'site'    => 'Garage',
+    'title'   => 'Settings',
+    'tab'     => 'Management',
+    'status'  => 'success',
+    'message' => 'Settings saved successfully',
+  ));
+});
 
 ?>
