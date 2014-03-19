0d449f923ae4219a83eda0be5543554769f990c8
Jason Rush <rush0033@hotmail.com>
2014-03-18 23:14:13 -0500

    Added a couple of files to copy

--- scripts/post-build.sh
+++ scripts/post-build.sh
@@ -6,5 +6,8 @@ rm -f $TARGETDIR/etc/os-release
 rm -f $TARGETDIR/etc/init.d/S20urandom
 
 cp $RPI_PATH/gomd/src/gomd.properties $TARGETDIR/etc/
+cp $RPI_PATH/gomd/src/gomd.properties $TARGETDIR/config/
 cp $RPI_PATH/gomd/src/gomd $TARGETDIR/usr/bin/
+
+cp -R $RPI_PATH/web/* $TARGETDIR/var/www/
 
